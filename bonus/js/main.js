c0c4823f4aac04bc13ff8352d6d48a31d9eba1bf
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-06-27 16:22:08 +0200

    feat(bonus): add condition to show list

---
+++ bonus/js/main.js
@@ -0,0 +1,33 @@
+// Descrizione:
+// Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+console.log("BONUS");
+
+const { createApp } = Vue;
+
+createApp ({
+    data() {
+        return {
+            emails: [],
+            allEmailsGenerated: false,
+        }
+    },
+    methods: {
+
+        addEmails() {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(risposta => {
+                    this.emails.push(risposta.data.response);
+                });
+            }
+        }
+
+    },
+    mounted() {
+        this.addEmails();
+    }
+
+}).mount('#app');
