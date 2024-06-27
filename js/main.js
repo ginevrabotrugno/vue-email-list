2709c954fa809d037e6be19191393a19d5b21e7e
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-06-27 15:24:22 +0200

    build: add base files

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Descrizione:
+// Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+const { createApp } = Vue;
+
+createApp ({
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+        
+    },
+    // mounted() {
+
+    //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    //     .then(risposta => {
+            
+    //     })
+    //     .catch(error => {
+    //     });
+        
+    // }
+}).mount('#app');
